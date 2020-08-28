9658e7ad9df309a2afdeb8566f6870a462a63c99
Gaurav Sharma <44618810+Gaurav9784@users.noreply.github.com>
2020-08-28 12:24:16 +0530

    Create Isomorphic tree.cpp

---
+++ Tree/Isomorphic tree.cpp
@@ -0,0 +1,16 @@
+bool isIsomorphic(Node *root1,Node *root2){
+    if(!root1&&!root2){
+        return true;
+    }
+    else if(root1&&root2){
+        if(root1->data==root2->data){
+            return (isIsomorphic(root1->left,root2->right)&&isIsomorphic(root1->left,root2->right))||(isIsomorphic(root1->left,root2->left)&&isIsomorphic(root1->right,root2->right));
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
